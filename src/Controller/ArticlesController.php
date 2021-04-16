49b8a50bc1e1a29df8192cc808ec99e3cd86b194
Théophile <theophile.noyer@hotmail.fr>
2021-04-16 15:23:00 +0200

    pagination articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Entity\Commentaires;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,13 +19,42 @@ class ArticlesController extends AbstractController
     /**
      * @Route("/articles", name="articles")
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator)
     {
-        //liste des articles
-        $articles = $this->getDoctrine()->getRepository(Articles::class)->findAll();
+        //liste des articles trié par date de création
+        $donnees = $this->getDoctrine()->getRepository(Articles::class)->findBy([],
+        ['created_at' => 'desc']);
+
+        //pagination
+        $articles = $paginator->paginate(
+            $donnees,
+            $request->query->getInt('page', 1),
+            4
+        );
 
         return $this->render('articles/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="slug")
+     */
+    public function article($slug, Request $request){
+        //on récupère l'article correspondant au slug
+        $article = $this->getDoctrine()->getRepository(Articles::class)->findOneBy(['slug' => $slug]);
+        $commentaires = $this->getDoctrine()->getRepository(Commentaires::class)->findBy([],
+            ['articles' => $article,
+            'actif' => 1],
+            ['created_at' => 'desc']
+        );
+        /*
+        if(!$article){
+
+        }
+        */
+        return $this->render('articles/article.html.twig', [
+            'article' => $article,
         ]);
     }
 }
